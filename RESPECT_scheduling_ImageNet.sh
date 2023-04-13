f9fad5a3a4df602fc497a72b41af1d1dbd1e881e
ycunxi <ycunxi@umass.edu>
2023-04-12 18:32:47 -0600

    Your commit message

--- RESPECT_scheduling_ImageNet.sh
+++ RESPECT_scheduling_ImageNet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-models='resnet101 densenet121 densenet169 densenet201 mobilenet_1.00_224 mobilenetv2_1.00_224 resnet101 resnet101v2 resnet152 resnet152v2'
+#models='resnet101 densenet121 densenet169 densenet201 mobilenet_1.00_224 mobilenetv2_1.00_224 resnet101 resnet101v2 resnet152 resnet152v2'
+models='resnet101'
 distributor="saved_model/run_20210827T155340/epoch-298.pt" 
 
 for model in $models
